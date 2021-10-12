ec928f627750d61882c47d149cb75dc82048c208
MichaelD <enee.kors@mail.ru>
2021-10-12 10:58:05 +0300

    Initial commit

---
+++ data-types.js
@@ -0,0 +1,23 @@
+const name = 'Yana'
+const age = 28
+const isProgrammer = true
+const favoriteDrink = null
+let address;
+const card = {
+    name: 'Yana',
+    age: 28,
+    isProgrammer: true
+}
+const id = Symbol('id')
+const bigIntNumber = 110n
+
+console.log('name', name, typeof name)
+console.log('age', age, typeof age)
+console.log('isProgrammer', isProgrammer, typeof isProgrammer)
+console.log('favoriteDrink', favoriteDrink, typeof favoriteDrink)
+console.log('address', address, typeof address)
+console.log('card', card, typeof card)
+console.log('id', Symbol('id'), typeof Symbol('id'))
+console.log('bigIntNumber', bigIntNumber, typeof bigIntNumber)
+
+
